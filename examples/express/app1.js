b9f43ab1ff1cc1e973a4bb93f2ed336196788131
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-12-24 05:37:27 +0200

    Updated express test

--- examples/express/app1.js
+++ examples/express/app1.js
@@ -1,9 +1,26 @@
-var deferred_listener = require('deferred-listener');
 var express = require('express');
+var deferred_listener = require('deferred-listener');
+var Q = require('q');
+var FS = require('fs');
+
+/* Let's create our middleware */
+function fetch_file(file, key) {
+	key = key || file;
+	var readFile = Q.nfbind(FS.readFile);
+	return deferred_listener(function(req, res) {
+		return readFile(file, "utf-8").then(function(data) {
+			if(!req.file) { req.file = {}; }
+			req.file[key] = JSON.parse(data);
+		});
+	});
+}
+
 var app = express();
 
-app.get('/hello.txt', function(req, res){
-	var body = 'Hello World';
+app.use( fetch_file("test.json", "test") );
+
+app.get('/index.html', function(req, res){
+	var body = 'test.foo = ' + req.file.test.foo + "\n";
 	res.setHeader('Content-Type', 'text/plain');
 	res.setHeader('Content-Length', body.length);
 	res.end(body);
